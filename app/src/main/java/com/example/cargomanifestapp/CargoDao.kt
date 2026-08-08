b56431ec946189e174412d8be8ca004c160eedbf
pacew-webhook <alamremix@gmail.com>
2026-08-09 00:53:49 +0900

    Update CargoDao.kt

--- app/src/main/java/com/example/cargomanifestapp/CargoDao.kt
+++ app/src/main/java/com/example/cargomanifestapp/CargoDao.kt
@@ -6,14 +6,17 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface CargoDao {
 
-    @Query("SELECT * FROM cargo_table")
+    @Query("SELECT * FROM cargo_table ORDER BY id ASC")
     fun getAllCargo(): Flow<List<CargoItem>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(cargoItem: CargoItem)
+    suspend fun insert(cargo: CargoItem)
+
+    @Update
+    suspend fun update(cargo: CargoItem)
 
     @Delete
-    suspend fun delete(cargoItem: CargoItem)
+    suspend fun delete(cargo: CargoItem)
 
     @Query("DELETE FROM cargo_table")
     suspend fun deleteAll()
