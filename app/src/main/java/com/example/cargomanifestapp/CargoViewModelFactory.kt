52f3513355db0fec95a194dd7b6ba6e0a3ef56d3
pacew-webhook <alamremix@gmail.com>
2026-08-09 08:33:48 +0900

    Update CargoViewModelFactory.kt

--- app/src/main/java/com/example/cargomanifestapp/CargoViewModelFactory.kt
+++ app/src/main/java/com/example/cargomanifestapp/CargoViewModelFactory.kt
@@ -1,34 +1,8 @@
 package com.example.cargomanifestapp
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
-
-class CargoViewModel(application: Application) : AndroidViewModel(application) {
-    private val dao = CargoDatabase.getDatabase(application).cargoDao()
-    
-    val cargoList: Flow<List<CargoItem>> = dao.getAllCargo()
-
-    fun insertCargo(item: CargoItem) = viewModelScope.launch {
-        dao.insert(item)
-    }
-
-    fun updateCargo(item: CargoItem) = viewModelScope.launch {
-        dao.update(item)
-    }
-
-    fun deleteCargo(item: CargoItem) = viewModelScope.launch {
-        dao.delete(item)
-    }
-
-    fun clearAllCargo() = viewModelScope.launch {
-        dao.deleteAll()
-    }
-}
 
 class CargoViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
