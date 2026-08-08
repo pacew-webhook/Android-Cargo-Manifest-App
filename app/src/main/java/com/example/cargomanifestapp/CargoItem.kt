cbec5ada462a2d9023ba9427556b43dd389523a5
pacew-webhook <alamremix@gmail.com>
2026-08-09 01:30:57 +0900

    Update CargoItem.kt

--- app/src/main/java/com/example/cargomanifestapp/CargoItem.kt
+++ app/src/main/java/com/example/cargomanifestapp/CargoItem.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "cargo_table")
 data class CargoItem(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val awbNo: String,
     val flightNo: String,
     val pti: String,
@@ -13,5 +14,6 @@ data class CargoItem(
     val weight: String,
     val subTotal: String,
     val description: String,
-    val customer: String
+    val customer: String,
+    val noPag: String = "" // <--- DITAMBAHKAN FIELD NO PAG
 )
