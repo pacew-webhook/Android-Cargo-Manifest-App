da011d1f06c0dd5cac128d7ba31b5ba1acb3871c
pacew-webhook <alamremix@gmail.com>
2026-08-09 18:20:05 +0900

    Update CargoViewModelFactory.kt

--- app/src/main/java/com/example/cargomanifestapp/CargoViewModelFactory.kt
+++ app/src/main/java/com/example/cargomanifestapp/CargoViewModelFactory.kt
@@ -4,11 +4,14 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class CargoViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+class CargoViewModelFactory(
+    private val application: Application,
+    private val cargoDao: CargoDao // Tambahkan ini
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CargoViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return CargoViewModel(application) as T
+            return CargoViewModel(application, cargoDao) as T // Tambahkan cargoDao di sini
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
