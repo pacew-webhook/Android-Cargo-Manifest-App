a50d9a01c805fe53af394c192aa1b5e992af7117
pacew-webhook <alamremix@gmail.com>
2026-08-09 18:59:19 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/cargomanifestapp/MainActivity.kt
+++ app/src/main/java/com/example/cargomanifestapp/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ContextHolder
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
