17afae32c14aab271b40ed1e167486d52e7c3487
pacew-webhook <alamremix@gmail.com>
2026-08-09 10:58:11 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,4 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
-id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false //
 }
