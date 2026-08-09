8b8af88eecc39bdddfbef3a341aff849a7d6c065
pacew-webhook <alamremix@gmail.com>
2026-08-10 00:38:46 +0900

    Update CargoDatabase.kt

--- app/src/main/java/com/example/cargomanifestapp/CargoDatabase.kt
+++ app/src/main/java/com/example/cargomanifestapp/CargoDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [CargoModel::class], version = 1, exportSchema = false) // <--- Ubah dari CargoItem ke CargoModel
+@Database(entities = [CargoItem::class], version = 1, exportSchema = false)
 abstract class CargoDatabase : RoomDatabase() {
     abstract fun cargoDao(): CargoDao
 
