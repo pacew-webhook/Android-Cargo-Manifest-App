18b05e545de9580b673717276a04b13c2a52e4dd
pacew-webhook <alamremix@gmail.com>
2026-08-11 09:51:44 +0900

    Update Screen.kt

--- app/src/main/java/com/example/cargomanifestapp/Screen.kt
+++ app/src/main/java/com/example/cargomanifestapp/Screen.kt
@@ -3,5 +3,6 @@ package com.example.cargomanifestapp
 enum class Screen {
     MAIN_MENU,
     MANIFEST_CARGO,
-    STOWING_PALLET
+    STOWING_PALLET,
+    BUKTI_TIMBANG // Tambahkan opsi menu baru ini
 }
