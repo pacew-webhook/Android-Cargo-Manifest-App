26f08a960b21906f95357a3c9f5af58e2b94516f
pacew-webhook <alamremix@gmail.com>
2026-08-11 14:51:33 +0900

    Update CargoDatabase.kt

--- app/src/main/java/com/example/cargomanifestapp/CargoDatabase.kt
+++ app/src/main/java/com/example/cargomanifestapp/CargoDatabase.kt
@@ -5,21 +5,20 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [CargoItem::class], version = 1, exportSchema = false)
+@Database(entities = [CargoItem::class], version = 2, exportSchema = false) // naikin jadi 2
 abstract class CargoDatabase : RoomDatabase() {
     abstract fun cargoDao(): CargoDao
 
     companion object {
-        @Volatile
-        private var INSTANCE: CargoDatabase? = null
+        @Volatile private var INSTANCE: CargoDatabase? = null
 
-        fun getDatabase(context: Context): CargoDatabase {
+        fun getInstance(context: Context): CargoDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     CargoDatabase::class.java,
-                    "cargo_database"
-                ).fallbackToDestructiveMigration().build()
+                    "cargo_db"
+                ).fallbackToDestructiveMigration().build() // ini biar ga usah uninstall
                 INSTANCE = instance
                 instance
             }
