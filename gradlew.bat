fe76f5076346ee4068f6380faf0c1615f91a78c6
pacew-webhook <alamremix@gmail.com>
2026-08-13 02:35:42 +0900

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,10 @@
+@echo off
+set DIRNAME=%~dp0
+set APP_HOME=%DIRNAME%
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+if defined JAVA_HOME (
+  set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+) else (
+  set JAVA_EXE=java.exe
+)
+"%JAVA_EXE%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
