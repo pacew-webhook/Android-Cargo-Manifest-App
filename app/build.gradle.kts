50ede73a760d95cb2478af4de3b7fe79e9470d79
pacew-webhook <alamremix@gmail.com>
2026-08-13 17:52:42 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.cargomanifestapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.13.5.2"
+        versionCode = 4
+        versionName = "1.13.5.4"
 
         val geminiApiKey = project.findProperty("GEMINI_API_KEY")?.toString() ?: ""
         buildConfigField("String", "GEMINI_API_KEY", "\"${geminiApiKey.replace("\"", "\\\"")}\"")
